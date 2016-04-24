914300e9bf55150da8e64efdd2a0c37b82a160fc
nokaa <nokaa@cock.li>
2016-04-24 12:23:22 -0500

    Initial working state

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate clap;
 use clap::{App, Arg};
 
 use std::{env, fs, process};
+use std::io::{self, Read, Write};
 
 fn main() {
     let matches = App::new("project-init")
@@ -37,18 +38,77 @@ fn main() {
     let dir = matches.is_present("directory");
     let cargo = matches.is_present("cargo");
 
-    println!("name: {}\tlicense: {}", name, license);
-    println!("dir: {}\tcargo: {}", dir, cargo);
-
     let config = config_dir();
+    let mut license_path = config.clone();
+    license_path.push_str(license);
 
-    //home.push(license);
-    let license_path = "";//home.to_str().unwrap();
-    if !file_exists(license_path) {
+    if !file_exists(&license_path[..]) {
         println!("License \"{}\" does not exist!", license);
         process::exit(1);
     }
-    println!("license exists!");
+    
+    // We make sure that the License exists before we create the
+    // directory
+    if !dir && !cargo { // Create directory manually
+        fs::create_dir_all(name).unwrap();
+        change_dir(name).unwrap();
+        fs::create_dir_all("src").unwrap();
+    } else if !dir && cargo { // using cargo
+        let cmd = process::Command::new("cargo")
+            .arg("new")
+            .arg("--bin")
+            .arg(name)
+            .output()
+            .expect("failed to execute cargo");
+
+        if !cmd.status.success() {
+            println!("stderr: {}", String::from_utf8_lossy(&cmd.stderr));
+            process::exit(1);
+        }
+
+        change_dir(name).unwrap();
+    } else if cargo { // use current directory and cargo
+        let cmd = process::Command::new("cargo")
+            .arg("init")
+            .arg("--bin")
+            .arg(name)
+            .output()
+            .expect("failed to execute cargo");
+
+        if !cmd.status.success() {
+            println!("stderr: {}", String::from_utf8_lossy(&cmd.stderr));
+            process::exit(1);
+        }
+    } else { // Use current directory manually
+        fs::create_dir_all("src").unwrap();
+    }
+
+    project_files(name, &license_path[..]);
+}
+
+fn project_files(name: &str, license_path: &str) {
+    copy_file(&license_path[..], "LICENSE").unwrap();
+    write_file("README.md", name.as_bytes()).unwrap();
+}
+
+fn read_file(filename: &str) -> Result<Vec<u8>, io::Error> {
+    let mut f = try!(fs::File::open(filename));
+    let mut buf: Vec<u8> = vec![];
+    try!(f.read_to_end(&mut buf));
+
+    Ok(buf)
+}
+
+fn write_file(filename: &str, data: &[u8]) -> Result<(), io::Error> {
+    let mut file = try!(fs::File::create(filename));
+    try!(file.write_all(data));
+    Ok(())
+}
+
+fn copy_file(filename: &str, dest: &str) -> Result<(), io::Error> {
+    let f = read_file(filename).unwrap();
+    try!(write_file(dest, &f[..]));
+    Ok(())
 }
 
 fn file_exists(filename: &str) -> bool {
@@ -60,13 +120,31 @@ fn file_exists(filename: &str) -> bool {
 
 fn config_dir() -> String {
     match env::var("XDG_CONFIG_HOME") {
-        Ok(s) => {
+        Ok(mut s) => {
+            if s.is_empty() {
+                s = get_home_dir();
+                s.push_str("/.config/");
+            } else if !s.ends_with('/') {
+                s.push('/');
+            }
+            s.push_str("license-add/");
             s
         }
         Err(_) => {
-            let mut home = env::home_dir().unwrap();
-            home.push(".config/");
-            home.to_str().unwrap().to_string()
+            let mut home = get_home_dir();
+            home.push_str("/.config/license-add/");
+            home
         }
     }
+}
+
+fn get_home_dir() -> String {
+    env::home_dir().unwrap().to_str().unwrap().to_string()
+}
+
+fn change_dir(dir: &str) -> io::Result<()> {
+    let mut current_dir = try!(env::current_dir());
+    current_dir.push(dir);
+    try!(env::set_current_dir(current_dir));
+    Ok(())
 }
